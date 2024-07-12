afb476d2a9b17951c7695e6606e6ba249a5ab468
Sujal Tangde <sujaltagade01@gmail.com>
2024-07-12 11:35:18 +0530

    gender section added

--- client/src/pages/Home/ShopByGender.jsx
+++ client/src/pages/Home/ShopByGender.jsx
@@ -15,23 +15,36 @@ export const ShopByGender = () => {
                     <div className='border border-gray-400 shadow shadow-gray-200'>
 
                         <div>
-                            <img src="/assets/images/man.webp" alt="man.webp" />
+                            <img src="/assets/images/man.jpg" alt="man.jpg" />
                         </div>
                         <div className='py-4 px-3  flex justify-between items-center'>
                         <span className='underline underline-offset-4'>MEN</span> <Link className=' bg-black text-white 
                             text-sm hover:bg-white hover:text-black px-4 py-2 border hover:border-black'>Explore More</Link>
                         </div>
                     </div>
+
                     <div className='border border-gray-400 shadow shadow-gray-200'>
 
                         <div>
-                            <img src="/assets/images/woman.webp" alt="woman.webp" />
+                            <img src="/assets/images/kid.jpg" alt="kid.jpg" />
+                        </div>
+                        <div className='py-4 px-3 flex justify-between items-center'>
+                            <span className='underline underline-offset-4'>KIDS</span> <Link className=' bg-black text-white 
+                            text-sm hover:bg-white hover:text-black px-4 py-2 border hover:border-black'>Explore More</Link>
+                        </div>
+                    </div>
+
+                    <div className='border border-gray-400 shadow shadow-gray-200'>
+
+                        <div>
+                            <img src="/assets/images/woman.jpg" alt="woman.jpg" />
                         </div>
                         <div className='py-4 px-3 flex justify-between items-center'>
                             <span className='underline underline-offset-4'>WOMEN</span> <Link className=' bg-black text-white 
                             text-sm hover:bg-white hover:text-black px-4 py-2 border hover:border-black'>Explore More</Link>
                         </div>
                     </div>
+                    
 
                 </div>
             </div>
